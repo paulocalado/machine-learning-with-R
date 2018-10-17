beb7a593bdd2337ea598a7ff6a1be85ecf9a2cb6
Paulo Henrique Calado Aoun <paulo_calado@live.com>
2018-10-16 22:50:23 -0300

    Initial commit

---
+++ DataFrames.R
@@ -0,0 +1,17 @@
+state.x77
+head(state.x77)
+tail(state.x77)
+
+days<- c('mon','tue','wed','thu','fri','sat','sun')
+temp<- c(87,65,78,88,92,77,65)
+rain<- c(T,F,T,T,F,F,T)
+
+df<- data.frame(days,temp,rain)
+
+df[1:4,c(1,3)]
+
+df$days #return a vector format
+df['days'] #return a data frame format
+
+condition<- rain==T
+subset(df,subset = condition)
