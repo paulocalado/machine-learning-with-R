f1777bd8f265e885ead0cd87500437e52a555536
Paulo Calado <paulo.calado@INLOCO0120.local>
2018-11-05 17:10:14 -0300

    overview do ggplot2

--- ggplot2Overview.R
+++ ggplot2Overview.R
@@ -1,2 +1,14 @@
 library(ggplot2)
-ggplot(data = mtcars)
+library(dplyr)
+
+#ggplot2 layers = Data, Aesthetics, Geometries, Facets, Statistics, Coordinates, Theme
+
+  mtcars %>%   # Data layer
+    ggplot() + # Data layer
+    aes(x=mpg,y=wt) + # Aesthetics Layer
+    geom_point() +  #Geometries Layer
+    facet_grid( cyl ~ .)+ #Facets Layer
+    stat_smooth() + # Statistics Layer
+    coord_cartesian(xlim = c(15,25)) + #Coordinates Layer
+    theme_bw()
+  
