c149a4809ad091e4448cd8e267e919ba7ff7b362
Paulo Calado <paulo.calado@INLOCO0120.local>
2018-11-08 11:34:11 -0300

    scatterplots

--- data_visualization/scatterplot.R
+++ data_visualization/scatterplot.R
@@ -7,3 +7,9 @@ df<- mtcars
 df %>%
   ggplot(aes(x=wt,y=mpg)) +
   geom_point(aes(shape=factor(cyl),color = factor(cyl)),size=3)
+
+
+df %>%
+  ggplot(aes(x=wt,y=mpg)) +
+  geom_point(aes(color=hp),size=5) +
+  scale_color_gradient(low = 'blue',high = 'red')
